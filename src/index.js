27d2a72ce26c414d4253ab897045e69817e38018
cvernikoff <cvernikoff@atlassian.com>
2021-08-13 15:55:20 -0700

    ga

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-5BVX10WN2K');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
     <React.StrictMode>
